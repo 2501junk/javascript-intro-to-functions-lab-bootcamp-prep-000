b80ce324411884a01b9822a06a8f33f8056f9a3a
jason dahm <2501junk@gmail.com>
2018-05-21 00:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 function shout(string){
-  return string.toUpperCase()
+  return shout.toUpperCase()
   return string
-  'Hello!'.toUpperCase()
+  //'Hello!'.toUpperCase()
 
-
-  var uppercase = "HELLO!"
-
-  uppercase.toUpperCase() === uppercase // true
-
-  var lowercase = 'hello!'
-
-  lowercase.toLowerCase() === lowercase // true
-
-  var mixedCase = 'Hi there!'
-
-  mixedCase.toLowerCase() === mixedCase // false
-
-  mixedCase.toUpperCase() === mixedCase // false
+  
 }
